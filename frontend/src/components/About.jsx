db5f4fad49732ae6974a3e5ebf71c426e2b86368
kidconcept <me@kidconcept.com>
2018-03-20 21:31:52 -0700

    added a placeholder page

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const About = () => {
+  return (
+    <div>
+      <h2>Hello, friend</h2>
+      <p>This place is a place, a representation of a place. It is the essence and substance from whence you came and to where henceforce you are destined to tread. Stay! Stay!</p>
+    </div>
+    )
+}
+
+export default About
